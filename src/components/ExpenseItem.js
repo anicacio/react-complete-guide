b756ba61415c9211580df41592ad63519dc380a0
Anderson <anderson@nicacio.com.br>
2021-08-09 00:48:51 -0300

    Working with dates on props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,17 @@
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
+  const month = props.date.toLocaleString('pt-BR', {month: 'long'});
+  const day = props.date.toLocaleString('pt-BR', {day: '2-digit'});
+  const year = props.date.getFullYear();
+
   return (
     <div className='expense-item'>
-      <div>{props.date.toISOString()}</div>
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
       <div className='expense-item__description'>
         <h2>{props.title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
