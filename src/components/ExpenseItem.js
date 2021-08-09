8122badfd6bffe0db4e6bd66f623e7d53fb69c26
Anderson <anderson@nicacio.com.br>
2021-08-09 00:12:05 -0300

    Using Dynamic data on components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,16 @@
 import './ExpenseItem.css'
 
 function ExpenseItem() {
+  const expanseDate = new Date(2021, 2, 28);
+  const expanseTitle = 'Car Insurance';
+  const expanseAmount = 294.67;
+
   return (
     <div className="expense-item">
-      <div>March 28th 2021</div>
+      <div>{expanseDate.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">$294.67</div>
+        <h2>{expanseTitle}</h2>
+        <div className="expense-item__price">${expanseAmount}</div>
       </div>
     </div>
   );
