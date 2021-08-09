334134da8f6d60bdc46ab4e8d6a2c912695a0864
Anderson <anderson@nicacio.com.br>
2021-08-09 00:40:15 -0300

    Using props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,12 @@
-import './ExpenseItem.css'
+import './ExpenseItem.css';
 
-function ExpenseItem() {
-  const expanseDate = new Date(2021, 2, 28);
-  const expanseTitle = 'Car Insurance';
-  const expanseAmount = 294.67;
-
+function ExpenseItem(props) {
   return (
-    <div className="expense-item">
-      <div>{expanseDate.toISOString()}</div>
-      <div className="expense-item__description">
-        <h2>{expanseTitle}</h2>
-        <div className="expense-item__price">${expanseAmount}</div>
+    <div className='expense-item'>
+      <div>{props.date.toISOString()}</div>
+      <div className='expense-item__description'>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
       </div>
     </div>
   );
