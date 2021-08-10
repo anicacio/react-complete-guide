0604d0963ed299597a00a7dc115aa87b2a75eb1c
Anderson <anderson@nicacio.com.br>
2021-08-09 22:45:56 -0300

    NewExpense form

---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,13 @@
+import "./NewExpense.css";
+import "./ExpenseForm";
+import ExpenseForm from "./ExpenseForm";
+
+const NewExpanse = () => {
+  return (
+    <div className="new-expense">
+      <ExpenseForm />
+    </div>
+  );
+};
+
+export default NewExpanse;
