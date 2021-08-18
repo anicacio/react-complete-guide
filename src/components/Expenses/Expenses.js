629421f9b2bf1327137ed49e8d486c9c368ba2ba
Anderson Nicacio <43139753+anicacio@users.noreply.github.com>
2021-08-18 01:14:52 -0300

    Understand "key" concept

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -18,6 +18,7 @@ const Expenses = (props) => {
       {console.log(props.items)}
       {props.items.map((expense) => (
         <ExpenseItem
+          key={expense.id}
           title={expense.title}
           amount={expense.amount}
           date={expense.date}
