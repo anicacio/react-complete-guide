c7f37ebef27dfef9d9cc2be6dcfaeb584b2357e9
Anderson Nicacio <43139753+anicacio@users.noreply.github.com>
2021-08-17 23:24:23 -0300

    Assignment ExpenseFilter

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,10 +1,17 @@
-import Card from "../UI/Card"
+import { useState } from "react";
+import Card from "../UI/Card";
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
+import ExpensesFilter from "./ExpensesFilter";
 
 const Expanses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2020");
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
   return (
     <Card className="expenses">
+      <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
       <ExpenseItem
         title={props.items[0].title}
         amount={props.items[0].amount}
@@ -27,6 +34,6 @@ const Expanses = (props) => {
       />
     </Card>
   );
-}
+};
 
 export default Expanses;
